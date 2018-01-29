708426d81bd9c7aeeeb205e196c6186cee8ccd0a
汝哲 <195440@qq.com>
2018-01-29 12:48:55 +0800

    webpace1=>3

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,10 @@ plugins.push(
 )
 
 module.exports = {
-  entry: ['./src/main.js'], //编译入口文件
+  entry: {
+    index: './src/main.js',
+  },
+ // ['./src/main.js'], //编译入口文件
   output: {
     publicPath: config.buildPath, //打包路径
     // path: path.resolve(__dirname + config.publicPath), //编译到app目录
@@ -44,29 +47,29 @@ module.exports = {
     filename: '[name].js?[hash]' //编译后的文件名
   },
   module: {
-    loaders: [{
+    rules: [{
         test: /\.js(x)*$/,
-        // exclude: /^node_modules$/,
-        loader: 'babel'
+       // exclude: /^node_modules$/,
+        loader: 'babel-loader'
       },
       {
         test: /\.vue$/,
-        loader: 'vue'
+        loader: 'vue-loader',
       },
       {
-        test: /\.css/,
-        exclude: /^node_modules$/,
-        loader: `style-loader!css-loader!autoprefixer-loader?{ browsers: ['last 100 versions'] }!`
+        test: /\.css$/,
+        //exclude: /^node_modules$/,
+        loader: `style-loader!css-loader`
       },
       {
         test: /\.less/,
-        exclude: /^node_modules$/,
-        loader: `style-loader!css-loader!autoprefixer-loader?{ browsers: ['last 100 versions'] }!less-loader`
+       // exclude: /^node_modules$/,
+        loader: `style-loader!css-loader!autoprefixer-loader!less-loader`
       },
       {
         test: /\.(png|jpg)$/,
         exclude: /^node_modules$/,
-        loader: 'url?limit=2000&name=[name].[ext]' //注意后面那个limit的参数，当你图片大小小于这个限制的时候，会自动启用base64编码图片
+        loader: 'url-loader?limit=2000&name=[name].[ext]' //注意后面那个limit的参数，当你图片大小小于这个限制的时候，会自动启用base64编码图片
       },
       {
         test: /\.(eot|woff|svg|ttf|woff2|gif|appcache)(\?|$)/,
@@ -77,18 +80,11 @@ module.exports = {
   },
   plugins,
   resolve: {
-    extensions: ['', '.js', '.vue', '.jsx'], //后缀名自动补全
+    extensions: ['.js', '.vue', '.jsx'], //后缀名自动补全
     alias: {
       vue: 'vue/dist/vue.js', //webpack打包时，需要设置别名
       store: path.resolve('src/store/'), //常用工具方法
     }
-  },
-  vue: {
-    postcss: [
-      require('autoprefixer')({
-        browsers: ['last 100 versions']
-      })
-    ]
   },
   devtool: 'source-map' // 开启代码地图
 }
